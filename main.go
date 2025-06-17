2989c389433ee583cee8647d55e74c334d250906
hasan-deriv <abuhasan@deriv.com>
2025-06-17 10:17:11 +0800

    added html template

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Serve static files
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Routes
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/article/", articleHandler)
+	http.HandleFunc("/admin", adminHandler)
+	http.HandleFunc("/new", newArticleHandler)
+	http.HandleFunc("/edit/", editArticleHandler)
+
+	log.Println("Server starting on :8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/home.html"))
+	tmpl.Execute(w, nil)
+}
+
+func articleHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/article.html"))
+	tmpl.Execute(w, nil)
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/admin.html"))
+	tmpl.Execute(w, nil)
+}
+
+func newArticleHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/new.html"))
+	tmpl.Execute(w, nil)
+}
+
+func editArticleHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/edit.html"))
+	tmpl.Execute(w, nil)
+} 
